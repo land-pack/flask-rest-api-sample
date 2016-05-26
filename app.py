7a0eb8cc905fe40849e15931330a60fe4b07043e
land-pack <landpack@sina.com>
2016-05-26 11:48:43 +0800

    #A Flask API Sample write by miguel blog!

---
+++ app.py
@@ -0,0 +1,132 @@
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'descripttion': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+# a helper function for generator a "public" version of a task to send to the client
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            # add a new field for index the task!
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/')
+def index():
+    return "Hello Frank"
+
+
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]  # loop for looking for the target element of list!
+    if len(task) == 0:  # If find one guy, but empty ,raise a 404 error to page!
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+
+    task = {
+        'id': tasks[-1]['id'] + 1,  # auto_increment like SQL
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+
+    if not request.json:
+        abort(400)
+
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthhorized access'}), 403)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
